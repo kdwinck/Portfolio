30faf0c09b5af1ad79f7e8b5f0d0aea0fdd69f47
Kyle Winckler <kyle.winckler@gmail.com>
2016-07-19 23:32:52 -0700

    started adding project constructor

---
+++ app.js
@@ -0,0 +1,15 @@
+////////////// Global Variables ///////////////////////////////////////////////
+var projectsArray = [];
+
+// This is where the project constructor will go
+//
+// It will be very similar to the article constructor used in the pair programming assignment
+
+function project(obj) = {
+  this.name = obj.name;
+  this.url = obj.url;
+  this.title = obj.title;
+  this.body = obj.body;
+  this.img = obj.img;
+  ///etc //// will determine what all to include later
+}
