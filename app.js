301fd538d3392250e513e0412366ac7daadb0494
Kyle Winckler <kyle.winckler@gmail.com>
2016-07-20 18:34:48 -0700

    continued working on constructor function and added blank html element for each project object

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 ////////////// Global Variables ///////////////////////////////////////////////
 var projectsArray = [];
 
-// This is where the project constructor will go
-//
-// It will be very similar to the article constructor used in the pair programming assignment
-
-function project(obj) = {
-  this.name = obj.name;
+function project(obj) {
   this.url = obj.url;
   this.title = obj.title;
   this.body = obj.body;
   this.img = obj.img;
-  ///etc //// will determine what all to include later
 }
