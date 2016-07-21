28d0098da7fc9f17002250dc21b9568fc66eedb0
Kyle Winckler <kyle.winckler@gmail.com>
2016-07-21 16:21:45 -0700

    created toHtml method to add projects to the page dynamically and added one project to the data file - some stylings added

---
+++ projects.js
@@ -0,0 +1,8 @@
+var projectData = [
+  {
+    title:  'Swift Me',
+    url:    'http://kdwinck.github.io/Swift-Me',
+    body:   '<p>Cheer captain 13 dvd Jaime King banjo dvd ex glowing in. The dark I remember this sick beat. Liars and the dirty dirty cheats Grammy oh. My god rosy cheeks see. Me again this love ex too late wonderstruck gym. Pie Ed Sheeran as I do. Girlnextdooritis ex someone else before forevermore make all the tables turn. Rose garden dancing on my own new romantics. Butterflies people say lying on your couch Emma Stone Katy Perry. Lily Aldridge vultures breakup screaming shame on me king bad. Guys vultures The Voice pictures in frames of kisses on cheeks. The struggle is real vultures good girl faith Emma Stone. Wrote you a song the fans ace. Meredith Grey oh my god the fans red lipstick 1989. Flashing lights 1989 rabbit hole banjo.</p>',
+    img:    'swiftcap.jpg'
+  }
+];
