e2a393e31503f11958be88f1cf16804568c671f7
Kyle Winckler <kyle.winckler@gmail.com>
2016-08-27 10:10:43 -0700

    begin deployment conversion

--- scripts/models/githubModel.js
+++ scripts/models/githubModel.js
@@ -5,13 +5,9 @@
   repos.all = [];
 
   repos.getData = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/user/repos?sort=updated&per_page=10',
-      type: 'GET',
-      headers: {
-        Authorization: 'token ' + githubToken
-      }
-    })
+    $.get('/github/users/kdwinck/repos' +
+          '?per_page=10' +
+          '$sort=updated')
     .done(function(data) {
       repos.all = data.filter(function(ele) {
         return ele.owner.login === 'kdwinck';
